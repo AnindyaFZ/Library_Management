6a2515b9c2c3d195719cce4b5e01b6c901152e2a
AnindyaFZ <anindyafarhan@gmail.com>
2024-06-13 21:38:58 +0600

    Scenebuilder Layout Formed

---
+++ src/main/java/com/anindya/library_management/LibraryManagementController.java
@@ -0,0 +1,42 @@
+package com.anindya.library_management;
+
+import javafx.scene.control.*;
+
+public class LibraryManagementController
+{
+    @javafx.fxml.FXML
+    private Label totalcopyLabel;
+    @javafx.fxml.FXML
+    private Label booknameLabel;
+    @javafx.fxml.FXML
+    private Label authornameLabel;
+    @javafx.fxml.FXML
+    private Button searchButton;
+    @javafx.fxml.FXML
+    private Label publicationLabel;
+    @javafx.fxml.FXML
+    private Label totalborrowedLabel;
+    @javafx.fxml.FXML
+    private CheckBox YesCheckBox;
+    @javafx.fxml.FXML
+    private Label membertypeLabel;
+    @javafx.fxml.FXML
+    private ComboBox membershipidComboBox;
+    @javafx.fxml.FXML
+    private Label totalmemberLabel;
+    @javafx.fxml.FXML
+    private ComboBox bookidComboBox;
+    @javafx.fxml.FXML
+    private RadioButton adultRadiobutton;
+    @javafx.fxml.FXML
+    private RadioButton childrenRadiobutton;
+    @javafx.fxml.FXML
+    private Label membernameLabel;
+    @javafx.fxml.FXML
+    private Label totalbookborrowedLabel;
+    @javafx.fxml.FXML
+    private Button borrowedbookButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
